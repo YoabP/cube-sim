098e6d363a0e9ddcc444d6bf5b73e0a2fad5fc4f
Yoab Pizarro <yoab.pizarro@gmail.com>
2016-10-17 05:29:17 -0500

    added depth to center axes

--- js/cube-sim/center.js
+++ js/cube-sim/center.js
@@ -10,35 +10,41 @@ CUBES.Center = class Center {
     this.rotationStep  = args.rotationStep? args.rotationStep : 1;
     this.orientationCount  = args.orientationCount? args.orientationCount : 0;
     this.orientation  = args.orientation? args.orientation : 0;
+    this.depth  = args.depth? args.depth : 1;
     this.piece  = args.piece? args.piece : null;
   }
-  rotate(CCW){
+  rotate(CCW, depth){
     //ClockWise: shift 0->1
     //counterClokwise: shift 0<-1
     var startingPiece;
     for (var r = 0; r < this.rotationStep; r++) {
       if(CCW){
-        startingPiece = this.sockets[0].piece;
-        for (var i = 0; i < this.sockets.length-1; i++) {
-          this.sockets[i].piece = this.sockets[i+1].piece;
+        startingPiece = this.sockets[depth][0].piece;
+        for (var i = 0; i < this.sockets[depth].length-1; i++) {
+          this.sockets[depth][i].piece = this.sockets[depth][i+1].piece;
         }
-        this.sockets[this.sockets.length-1].piece = startingPiece;
+        this.sockets[depth][this.sockets[depth].length-1].piece = startingPiece;
       }
       else{
-        startingPiece = this.sockets[this.sockets.length-1].piece;
-        for (var i = this.sockets.length-2; i >= 0; i--) {
-          this.sockets[i+1].piece = this.sockets[i].piece;
+        startingPiece = this.sockets[depth][this.sockets[depth].length-1].piece;
+        for (var i = this.sockets[depth].length-2; i >= 0; i--) {
+          this.sockets[depth][i+1].piece = this.sockets[depth][i].piece;
         }
-        this.sockets[0].piece = startingPiece;
+        this.sockets[depth][0].piece = startingPiece;
       }
     }
   }
   log(){
     var str = '[';
-    var len = this.sockets.length;
-    this.sockets.forEach(function(elem, index){
-      str += elem.piece + (index == len-1?'':',');
-    });
+    var depth = this.sockets.length;
+    for (var i = 0; i < depth; i++) {
+      var len = this.sockets[i].length;
+      str = '[';
+      this.sockets[i].forEach(function(elem, index){
+        str += elem.piece + (index == len-1?'':',');
+      });
+      str+=']';
+    }
     str+=']';
     console.log(str);
   }
