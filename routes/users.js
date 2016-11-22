d9f925cc0835cd581782fe5cb092d6e6b86108ea
ypizarro <ypizarro>
2016-11-22 11:49:54 -0600

    pc crashed, moving on

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,9 @@ router.get('/', function(req, res, next) {
   var db = req.db;
   var collection = db.get('usersCollection');
 
-  collection.find({},{},function(e,docs){
+  collection.find({},{},function(err,docs){
+
+      console.log(err);
         res.json(docs);
     });
 });
@@ -17,30 +19,58 @@ router.get('/', function(req, res, next) {
 //register
 //TODO validate duplicates
 router.post('/new', function(req, res, next){
+  console.log(req,res,next);
   var db = req.db;
-  var collection = db.get('usersCollection');
+  console.log("DB");
+  var User = db.get('usersCollection');
+  console.log("collection");
   var password = req.body.password;
   var email = req.body.email;
   var name = req.body.name;
+  console.log("params");
   var passData = encrypt(password);
-  collection.insert({
-    "name" : name,
-    "email" : email,
-    "passData" : passData
-  }, function (err, doc) {
-      if (err) {
-          // If it failed, return error
-          res.status(404).json(err);
-          return;
-      }
-      else {
-          // return insterted
-          var token = generateJwt(doc);
-          res.status(200);
-          res.json({
-            "token" : token
+console.log("encrypt()");
+  var exists = false;
+  User.findOne({ email: email }, function (err, user) {
+    console.log(err);
+    if (err) { res.status(404).json(err); return;}
+    console.log("Mail");
+    if (!user) {
+      User.findOne({ name: name }, function (err, user) {
+        console.log(err);
+        if (err) { res.status(404).json(err); return;}
+        console.log("name");
+        if (!user) {
+          User.insert({
+            "name" : name,
+            "email" : email,
+            "passData" : passData
+          }, function (err, doc) {
+              if (err) {
+                  // If it failed, return error
+                  console.log("fail");
+                  res.status(404).json(err);
+              }
+              else {
+                console.log("done");
+                  // return insterted
+                  var token = generateJwt(doc);
+                  res.status(200);
+                  res.json({
+                    "token" : token
+                  });
+              }
           });
-      }
+        }
+        else{
+          // Return if name is duplicate.
+          res.status(404).json({message: "UserName already in use."})
+        }
+      });
+    }else{
+      // Return if email is duplicate.
+      res.status(404).json({message: "Email already in use."})
+    }
   });
 });
 router.post('/login', function(req, res, next){
@@ -68,11 +98,13 @@ router.post('/login', function(req, res, next){
 function encrypt(password){
   var r = {};
   r.salt = crypto.randomBytes(16).toString('hex');
-  r.hash = crypto.pbkdf2Sync(password, r.salt, 1000, 64).toString('hex');
+  r.hash = crypto.pbkdf2Sync('secret', 'salt', 1000, 64, 'sha1').toString('hex');
+
   return r;
 };
 function validPassword(password, passData){
-  var hash = crypto.pbkdf2Sync(password, passData.salt, 1000, 64).toString('hex');
+  var hash = crypto.pbkdf2Sync('secret', 'salt', 1000, 64, 'sha1').toString('hex');
+
   return passData.hash === hash;
 }
 function generateJwt(user) {
