2ea304d45bfb6853d16f33809921f52ca354199a
Yoab Pizarro <yoab.pizarro@gmail.com>
2016-11-27 23:08:09 -0600

    Restore before scramble, & fisher camera

--- js/cube-sim/Fisher333/fisher333Controller.js
+++ js/cube-sim/Fisher333/fisher333Controller.js
@@ -6,4 +6,10 @@
  * @extends {CUBES.Cube333.Controller}
  */
 CUBES.Fisher333.Controller = class Controller extends CUBES.Cube333.Controller{
+  constructor(camera, controls, view) {
+    super(camera, controls, view);
+    // Move camera so cube is seen from the front at begging
+    this.controls.position0.set(-3.5, 0, 3.5);
+    this.controls.reset();
+  }
 }
