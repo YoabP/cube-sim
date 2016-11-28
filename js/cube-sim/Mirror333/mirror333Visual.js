bb76d49696bd2e9a32ade3818c77eb3d1395ee17
Yoab Pizarro <yoab.pizarro@gmail.com>
2016-11-27 19:42:10 -0600

    Mirror cube

---
+++ js/cube-sim/Mirror333/mirror333Visual.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * Visualization of the 333 Mirror puzzle.
+ * Needs a logical model and scene to work.
+ * @class
+ */
+CUBES.Mirror333.View = class View extends CUBES.Cube333.View{}
